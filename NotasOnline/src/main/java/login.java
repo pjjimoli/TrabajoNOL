a3dbc948c85509fd49f64171cf6749e61702d65e
user <user@ds-dew-110.dsicv.upv.es>
2024-05-22 21:23:00 +0200

    HITO 2

--- NotasOnline/src/main/java/login.java
+++ NotasOnline/src/main/java/login.java
@@ -83,102 +83,17 @@ public class login extends HttpServlet {
 		            key += responseLine; // Concatenamos cada línea a la cadena key
 		        }
 		    }
-		    
+		    if (!key.equals("-1")) {
+				if (request.isUserInRole("rolpro")) {
+					htmlProf(response, dni, key, cookies);
+				} else if (request.isUserInRole("rolalu")) {
+					htmlAlu(response, dni, key, cookies);
+				} else {
+					response.sendError(401);
+				}
+			}
 		}
-		List<AsignaturasAlumn> asignas = getAsignAlumn(dni, key, cookies);
-		response.setContentType("text/html");
-		response.getWriter()
-		.println("<!DOCTYPE html>\n<html>\n<head>\n"
-				+ "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\n"
-				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
-				+ "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n"
-				+ "<link href=estilo.css rel=\"stylesheet\">\n"
-				+ "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\n"
-				+ "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>"
-				+ "<title>Alumno - Notas Online</title></head>\n");
-		response.getWriter()
-		.println("<body>\r\n"
-				+ "<form action=\"login\" method=\"get\">\r\n"
-				+ "              <input class=\"logout\" type=\"submit\" value=\"Cerrar Sesión\">\r\n"
-				+ "          </form>\r\n"
-				+ "          \r\n"
-				+ "	<main class=\"container-fluid\">\r\n"
-				+ "		<div class=\"p-4 p-md-5 mb-4 main\">\r\n"
-				+ "			<div class=\"mx-auto p-1 center\">\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "				<h1 class=\"display-4 fw-bold\">NOTAS ONLINE</h1>\r\n"
-				+ "				<p class=\"lead my-1\">"+ getAlu(dni, key, cookies).getNombre() +" "+ getAlu(dni, key, cookies).getApellidos()+ "</p>\r\n"
-				+ "				<p class=\"lead my-3\">Aquí se muestran las asignaturas en las que\r\n"
-				+ "					estás matriculado junto a sus notas</p>\r\n"
-				+ "			</div>\r\n"
-				+ "		</div>\r\n"
-				+ "		<div class=\"row g-5\">\r\n"
-				+ "			<div class=\"col-md-8\">\r\n"
-				+ "			\r\n"
-				+ "				<div class=\"card text-center tabs\">\r\n"
-				+ "    <div class=\"card-header \">\r\n"
-				+ "    <ul class=\"nav nav-tabs card-header-tabs \">\r\n");
-				response.getWriter()
-				.println("<li class=\"nav-item \">\r\n"
-				+ "        <a class=\"nav-link active unselected-tab\" id=\"a0-tab\" data-toggle=\"tab\" href=\"#a0\" role=\"tab\" aria-controls=\"a0\" aria-selected=\"true\">" + asignas.get(0).getNombre() + "</a>\r\n"
-				+ "      </li>\r\n");
-				for (int i = 1; i < asignas.size(); i++) {
-					response.getWriter().println("<li class=\"nav-item\">\r\n"
-							+ "        <a class=\"nav-link unselected-tab\" id=\"a " + i + "-tab\" data-toggle=\"tab\" href=\"#a"+ i +"\" role=\"tab\" aria-controls=\"a"+ i +"\" aria-selected=\"false\">"+asignas.get(i).getNombre()+"</a>\r\n"
-							+ "      </li>\r\n");
-				}
-				Asignaturas primera = getInfoAsignatura(asignas.get(0).getNombre(), key, cookies);
-				response.getWriter().println("    </ul>\r\n"
-				+ "  </div>\r\n"
-				+ "  \r\n"
-				+ "  \r\n"
-				+ "  <div class=\"card-body\">\r\n"
-				+ "                    <div class=\"tab-content\" id=\"myTabContent\">\r\n"
-				+ "                        <div class=\"tab-pane fade show active\" id=\"a0\" role=\"tabpanel\" aria-labelledby=\"a0-tab\">\r\n"
-				+ "                          <table>\r\n"
-				+ "                            <tr><th>Nombre</th><th>Nota</th><th>Curso</th><th>Cuatrimestre</th><th>Créditos</th></tr>\r\n"
-				+ "                            <tr><td>"+primera.getNombre()+"</td><td>"+asignas.get(0).getNota()+"</td><td>"+primera.getCurso()+"</td><td>"+primera.getCuatrimestre()+"</td><td>"+primera.getCreditos()+"</td></tr>\r\n"
-				+ "                        </table>\r\n"
-				+ "                        </div>\r\n");
-				for (int i = 1; i < asignas.size(); i++) {
-					Asignaturas asi = getInfoAsignatura(asignas.get(i).getNombre(), key, cookies);
-					response.getWriter().println(
-					"                        <div class=\"tab-pane fade show active\" id=\"a"+i+"\" role=\"tabpanel\" aria-labelledby=\"a"+i+"-tab\">\r\n"
-					+ "                          <table>\r\n"
-					+ "                            <tr><th>Nombre</th><th>Nota</th><th>Curso</th><th>Cuatrimestre</th><th>Créditos</th></tr>\r\n"
-					+ "                            <tr><td>"+asi.getNombre()+"</td><td>"+asignas.get(i).getNota()+"</td><td>"+asi.getCurso()+"</td><td>"+asi.getCuatrimestre()+"</td><td>"+asi.getCreditos()+"</td></tr>\r\n"
-					+ "                        </table>\r\n"
-					+ "                        </div>\r\n");
-				}
-				response.getWriter().println( "                    </div>\r\n"
-				+ "                </div>\r\n"
-				+ "  \r\n"
-				+ "  \r\n"
-				+ "</div>\r\n"
-				+ "				\r\n"
-				+ "			</div>\r\n"
-				+ "			<div class=\"col-md-4\">\r\n"
-				+ "				<div class=\"p-4 mb-3 grupo\">\r\n"
-				+ "					<h4 class=\"center\">GRUPO 3TI21_G2</h4>\r\n"
-				+ "					<ul>\r\n"
-				+ "						<li>Blauvac Brea, Adrián Pierre</li>\r\n"
-				+ "						<li>García Bartolomé, Javier</li>\r\n"
-				+ "						<li>Jiménez Olivares, Pedro José</li>\r\n"
-				+ "						<li>Moris Puig, Yvan</li>\r\n"
-				+ "						<li>Rea Mejia, Maria Carmen</li>\r\n"
-				+ "						<li>Trull Martí, Andreu</li>\r\n"
-				+ "					</ul>\r\n"
-				+ "				</div>\r\n"
-				+ "			</div>\r\n"
-				+ "		</div>\r\n"
-				+ "	</main>\r\n"
-				+ "	<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\r\n"
-				+ "</body>\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "</html>");
+
 	}
 
 	private Alumno getAlu(String user,
@@ -218,7 +133,157 @@ public class login extends HttpServlet {
 
 		return alumno;
 	}
+	private void htmlProf(HttpServletResponse response, String dni, String key, List<String> cookies) throws MalformedURLException, IOException {
+		response.setContentType("text/html");
+		response.getWriter()
+		.println("<!DOCTYPE html>\n<html>\n<head>\n"
+				+ "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\n"
+				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+				+ "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n"
+				+ "<link href=estilo.css rel=\"stylesheet\">\n"
+				+ "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\n"
+				+ "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>"
+				+ "<title>Profesor - Notas Online</title></head>\n");
+		response.getWriter()
+		.println("<body>\r\n"
+				+ "<form action=\"login\" method=\"get\">\r\n"
+				+ "              <input class=\"logout\" type=\"submit\" value=\"Cerrar Sesión\">\r\n"
+				+ "          </form>\r\n"
+				+ "          \r\n"
+				+ "	<main class=\"container-fluid\">\r\n"
+				+ "		<div class=\"p-4 p-md-5 mb-4 main\">\r\n"
+				+ "			<div class=\"mx-auto p-1 center\">\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "				<h1 class=\"display-4 fw-bold\">PRÓXIMAMENTE</h1>\r\n"
+				+ "				<p class=\"lead my-3\">Aquí podrás consultar y modificar las notas de tus alumnos en un futuro</p>\r\n"
+				+ "			</div>\r\n"
+				+ "		</div>\r\n"
+				+ "		<div class=\"row g-5\">\r\n"
+				+ "			<div class=\"col-md-8\">\r\n"
+				+ "			\r\n"
+				+ "			<div class=\"col-md-4\">\r\n"
+				+ "				<div class=\"p-4 mb-3 grupo\">\r\n"
+				+ "					<h4 class=\"center\">GRUPO 3TI21_G2</h4>\r\n"
+				+ "					<ul>\r\n"
+				+ "						<li>Blauvac Brea, Adrián Pierre</li>\r\n"
+				+ "						<li>García Bartolomé, Javier</li>\r\n"
+				+ "						<li>Jiménez Olivares, Pedro José</li>\r\n"
+				+ "						<li>Moris Puig, Yvan</li>\r\n"
+				+ "						<li>Rea Mejia, Maria Carmen</li>\r\n"
+				+ "						<li>Trull Martí, Andreu</li>\r\n"
+				+ "					</ul>\r\n"
+				+ "				</div>\r\n"
+				+ "			</div>\r\n"
+				+ "		</div></div>\r\n"
+				+ "	</main>\r\n"
+				+ "	<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\r\n"
+				+ "</body>\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "</html>");
+	}
 	
+	private void htmlAlu(HttpServletResponse response,String dni, String key, List<String> cookies) throws MalformedURLException, IOException{
+		List<AsignaturasAlumn> asignas = getAsignAlumn(dni, key, cookies);
+		response.setContentType("text/html");
+		response.getWriter()
+		.println("<!DOCTYPE html>\n<html>\n<head>\n"
+				+ "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" />\n"
+				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+				+ "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n"
+				+ "<link href=estilo.css rel=\"stylesheet\">\n"
+				+ "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\n"
+				+ "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>"
+				+ "<title>Alumno - Notas Online</title></head>\n");
+		response.getWriter()
+		.println("<body>\r\n"
+				+ "<form action=\"login\" method=\"get\">\r\n"
+				+ "              <input class=\"logout\" type=\"submit\" value=\"Cerrar Sesión\">\r\n"
+				+ "          </form>\r\n"
+				+ "          \r\n"
+				+ "	<main class=\"container-fluid\">\r\n"
+				+ "		<div class=\"p-4 p-md-5 mb-4 main\">\r\n"
+				+ "			<div class=\"mx-auto p-1 center\">\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "				<h1 class=\"display-4 fw-bold\">NOTAS ONLINE</h1>\r\n"
+				+ "				<p class=\"lead my-1\">"+ getAlu(dni, key, cookies).getNombre() +" "+ getAlu(dni, key, cookies).getApellidos()+ "</p>\r\n"
+				+ "				<p class=\"lead my-3\">Aquí se muestran las asignaturas en las que\r\n"
+				+ "					estás matriculado junto a sus notas</p>\r\n"
+				+ "			</div>\r\n"
+				+ "		</div>\r\n"
+				+ "		<div class=\"row g-5\">\r\n"
+				+ "			<div class=\"col-md-8\">\r\n"
+				+ "			\r\n"
+				+ "				<div class=\"card text-center tabs\">\r\n"
+				+ "    <div class=\"card-header \">\r\n"
+				+ "    <ul class=\"nav nav-tabs card-header-tabs \">\r\n");
+				response.getWriter()
+				.println("<li class=\"nav-item \">\r\n"
+				+ "        <a class=\"nav-link active unselected-tab\" id=\"a1-tab\" data-toggle=\"tab\" href=\"#a1\" role=\"tab\" aria-controls=\"a1\" aria-selected=\"true\">" + asignas.get(0).getNombre() + "</a>\r\n"
+				+ "      </li>\r\n");
+				for (int i = 1; i < asignas.size(); i++) {
+					response.getWriter().println("<li class=\"nav-item\">\r\n"
+							+ "        <a class=\"nav-link unselected-tab\" id=\"a " + (i+1) + "-tab\" data-toggle=\"tab\" href=\"#a"+ (i+1) +"\" role=\"tab\" aria-controls=\"a"+ (i+1) +"\" aria-selected=\"false\">"+asignas.get(i).getNombre()+"</a>\r\n"
+							+ "      </li>\r\n");
+				}
+				Asignaturas primera = getInfoAsignatura(asignas.get(0).getNombre(), key, cookies);
+				response.getWriter().println("    </ul>\r\n"
+				+ "  </div>\r\n"
+				+ "  \r\n"
+				+ "  \r\n"
+				+ "  <div class=\"card-body\">\r\n"
+				+ "                    <div class=\"tab-content\" id=\"myTabContent\">\r\n"
+				+ "                        <div class=\"tab-pane fade show active\" id=\"a1\" role=\"tabpanel\" aria-labelledby=\"a1-tab\">\r\n"
+				+ "                          <table>\r\n"
+				+ "                            <tr><th>Nombre</th><th>Nota</th><th>Curso</th><th>Cuatrimestre</th><th>Créditos</th></tr>\r\n"
+				+ "                            <tr><td>"+primera.getNombre()+"</td><td>"+asignas.get(0).getNota()+"</td><td>"+primera.getCurso()+"</td><td>"+primera.getCuatrimestre()+"</td><td>"+primera.getCreditos()+"</td></tr>\r\n"
+				+ "                        </table>\r\n"
+				+ "                        </div>\r\n");
+				
+				 for (int i = 1; i < asignas.size(); i++) {
+				 
+					Asignaturas asi = getInfoAsignatura(asignas.get(i).getNombre(), key, cookies);
+					response.getWriter().println(
+					"                        <div class=\"tab-pane fade show active\" id=\"a"+(i+1)+"\" role=\"tabpanel\" aria-labelledby=\"a"+(i+1)+"-tab\">\r\n"
+					+ "                          <table>\r\n"
+					+ "                            <tr><th>Nombre</th><th>Nota</th><th>Curso</th><th>Cuatrimestre</th><th>Créditos</th></tr>\r\n"
+					+ "                            <tr><td>"+asi.getNombre()+"</td><td>"+asignas.get(i).getNota()+"</td><td>"+asi.getCurso()+"</td><td>"+asi.getCuatrimestre()+"</td><td>"+asi.getCreditos()+"</td></tr>\r\n"
+					+ "                        </table>\r\n"
+					+ "                        </div>\r\n");
+				}
+				
+				response.getWriter().println( "                    </div>\r\n"
+				+ "                </div>\r\n"
+				+ "  \r\n"
+				+ "  \r\n"
+				+ "</div>\r\n"
+				+ "				\r\n"
+				+ "			</div>\r\n"
+				+ "			<div class=\"col-md-4\">\r\n"
+				+ "				<div class=\"p-4 mb-3 grupo\">\r\n"
+				+ "					<h4 class=\"center\">GRUPO 3TI21_G2</h4>\r\n"
+				+ "					<ul>\r\n"
+				+ "						<li>Blauvac Brea, Adrián Pierre</li>\r\n"
+				+ "						<li>García Bartolomé, Javier</li>\r\n"
+				+ "						<li>Jiménez Olivares, Pedro José</li>\r\n"
+				+ "						<li>Moris Puig, Yvan</li>\r\n"
+				+ "						<li>Rea Mejia, Maria Carmen</li>\r\n"
+				+ "						<li>Trull Martí, Andreu</li>\r\n"
+				+ "					</ul>\r\n"
+				+ "				</div>\r\n"
+				+ "			</div>\r\n"
+				+ "		</div>\r\n"
+				+ "	</main>\r\n"
+				+ "	<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\r\n"
+				+ "</body>\r\n"
+				+ "\r\n"
+				+ "\r\n"
+				+ "</html>");
+	}
 	
 	private List<AsignaturasAlumn> getAsignAlumn(String dni, String key, List<String> cookies)
 			throws MalformedURLException, IOException {
